fadf49abd3281e26e3afd34b7d3e939e808cbf07
Ascecent <andres85044@gmail.com>
2021-07-24 23:58:26 -0500

    First commit

---
+++ js/index.js
@@ -0,0 +1,19 @@
+const navLinks = document.getElementById("navLinks");
+const htmlDoc = document.getElementsByTagName("html")[0];
+
+function openMenu() {
+
+    /* Set the page scroll to zero */
+    window.scroll({
+        top: 0,
+        behavior: "smooth"
+    });
+    
+    htmlDoc.style.overflow = "hidden";
+    navLinks.style.right= "0";
+}
+
+function closeMenu() {
+    htmlDoc.style.overflow = "auto";
+    navLinks.style.right = "-15rem";
+}
