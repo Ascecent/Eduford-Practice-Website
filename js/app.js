0d5c625711f517b03a615df2a5c6faa92bfc6c0e
Ascecent <andres85044@gmail.com>
2021-07-26 14:51:13 -0500

    Updating project

--- js/app.js
+++ js/app.js
@@ -2,18 +2,28 @@ const navLinks = document.getElementById("navLinks");
 const htmlDoc = document.getElementsByTagName("html")[0];
 
 function openMenu() {
+  /* Set the page scroll to zero */
+  window.scroll({
+    top: 0,
+    behavior: "smooth",
+  });
 
-    /* Set the page scroll to zero */
-    window.scroll({
-        top: 0,
-        behavior: "smooth"
-    });
-    
-    htmlDoc.style.overflow = "hidden";
-    navLinks.style.right= "0";
+  htmlDoc.style.overflow = "hidden";
+  navLinks.style.right = "0";
 }
 
 function closeMenu() {
-    htmlDoc.style.overflow = "auto";
-    navLinks.style.right = "-15rem";
+  htmlDoc.style.overflow = "auto";
+  navLinks.style.right = "-15rem";
+}
+
+/* Slown down scroll function */
+
+const main = document.getElementById("main");
+
+function slowDownScroll() {
+  window.scroll({
+    top: main.offsetTop,
+    behavior: "smooth",
+  });
 }
